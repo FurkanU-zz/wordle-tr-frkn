ebb50569004831342c21b9fe80875b978cb1d49b
Furkan Uyanık <furkannuyanik@gmail.com>
2024-05-28 01:04:39 +0300

    Update words.ts

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -12,19 +12,13 @@ export const isWinningWord = (word: string) => {
   return solution === word
 }
 
-export const getWordOfDay = () => {
-  // January 1, 2022 Game Epoch
-  const epochMs = new Date('January 1, 2022 00:00:00').valueOf()
-  const now = Date.now()
-  const msInDay = 86400000
-  const index = Math.floor((now - epochMs) / msInDay)
-  const nextday = (index + 1) * msInDay + epochMs
-
+export const getRandomWord = () => {
+  const index = Math.floor(Math.random() * WORDS.length);
+  
   return {
-    solution: WORDS[index % WORDS.length].toUpperCase(),
+    solution: WORDS[index].toUpperCase(),
     solutionIndex: index,
-    tomorrow: nextday,
   }
 }
 
-export const { solution, solutionIndex, tomorrow } = getWordOfDay()
+export const { solution, solutionIndex } = getRandomWord();
